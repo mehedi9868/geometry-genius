ba41dc2f4cbe94e41cb5a0c769513108f51e76a0
mehedi9868 <mehedi9868@gmail.com>
2023-02-17 21:58:13 +0600

    header section done

---
+++ js/blog.js
@@ -0,0 +1,8 @@
+document.getElementById('blog-btn').addEventListener('click', function () {
+    // go to blog
+    window.location.href = 'blog.html';
+})
+document.getElementById('header-brand-text').addEventListener('click', function () {
+    // back to home
+    window.location.href = 'index.html';
+})
